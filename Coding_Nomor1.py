6ad09d1ff54a5139cc0e2f5c923942d96f9f3bda
safiiraaulia <112611988+safiiraaulia@users.noreply.github.com>
2024-04-28 12:21:46 +0700

    Update Coding_Nomor1.py

--- Coding_Nomor1.py
+++ Coding_Nomor1.py
@@ -11,3 +11,45 @@ df.info()
 
 # Informasi statistik data
 df.describe()
+
+# Menghitung jumlah
+total = df['Video Uploads'].sum()
+
+# Menghitung rata-rata
+mean = df['Video Uploads'].mean()
+
+# Menghitung median
+median = df['Video Uploads'].median()
+
+print("Video Uploads")
+print("Jumlah:", total)
+print("Rata-rata:", mean)
+print("Median:", median)
+
+# Menghitung jumlah
+total = df['Video views'].sum()
+
+# Menghitung rata-rata
+mean = df['Video views'].mean()
+
+# Menghitung median
+median = df['Video views'].median()
+
+print("Video views")
+print("Jumlah:", total)
+print("Rata-rata:", mean)
+print("Median:", median)
+
+# Menghitung jumlah
+total = df['Subscribers'].sum()
+
+# Menghitung rata-rata
+mean = df['Subscribers'].mean()
+
+# Menghitung median
+median = df['Subscribers'].median()
+
+print("Subscribers")
+print("Jumlah:", total)
+print("Rata-rata:", mean)
+print("Median:", median)
